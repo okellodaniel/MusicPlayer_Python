4e5cd18830e6997f341581925006b7043927e9ad
Daniel Okello <handsdani@gmail.com>
2022-01-01 23:57:00 +0300

    Added functions for Pausing,resume,play,stop

--- player.py
+++ player.py
@@ -49,3 +49,71 @@ Resume_button.grid['font'] = defined_font
 Resume_button.grid(row=1, column=3)
 
 # Previous Button
+previous_button = Button(root, text="Previous", width=7, command=Previous)
+previous_button.grid['font'] = defined_font
+previous_button.grid(row=1, column=4)
+
+# Next Button
+next_button = Button(root, text="Next", width=7, command=Next)
+next_button.grid['font'] = defined_font
+next_button.grid(row=1, column=5)
+
+# Menu
+my_menu = Menu(root)
+root.config(menu=my_menu)
+my_menu.add_cascade(label="Menu", menu=add_song_menu)
+add_song_menu.add_command(label="Add songs", command=addsongs)
+add_song_menu.add_command(label="Delete song", command=deletesong)
+
+mainloop()
+
+
+# Function to play the song
+
+# Adding many songd to Playlist
+
+def addsongs():
+
+    # Open a file dialog
+
+    temp_song = filedialog.askopenfilenames(initialdir="/home/downloads/music",
+                                            title="Select a Song", filetypes=(("mp3 Files", "*.mp3"),))
+
+    # Add the song to the listbox
+
+    for song in temp_song:
+        song = song.replace("/home/downloads/music/", "")
+        song_list.insert(END, song)
+
+
+def deletesong():
+    # Delete the selected song from the listbox
+
+    curr_song = song_list.curselection()
+    song_list.delete(curr_song[0])
+
+
+def play():
+    song = songs_list.get(ACTIVE)
+    song = f"/home/downloads/music/{song}"
+    mixer.music.load(song)
+    mixer.music.play()
+
+# Function to pause the song
+
+
+def Pause():
+    mixer.music.pause()
+
+# Function to resume the song
+
+
+def Resume():
+    mixer.music.unpause()
+
+
+# Function to stop the song
+
+def Stop():
+    mixer.music.stop()
+    song_list.selection_clear(ACTIVE)
